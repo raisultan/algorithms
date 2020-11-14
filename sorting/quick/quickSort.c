65afa9952511e738f96aed60b7ccd8650bb641b9
raisultan <ki.xbozz@gmail.com>
2020-11-14 14:35:20 +0700

    quick sort - c

--- sorting/quick/quickSort.c
+++ sorting/quick/quickSort.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 void swap(int *a, int *b) {
 	int temp = *a;
@@ -8,58 +6,29 @@ void swap(int *a, int *b) {
 	*b = temp;
 }
 
-int partition(int *array, int lo, int hi, int pivot) {
-	while(1) {
-		printf("\n");
-		for(int i = 0; i < pivot; i++) {
-			printf("%d ", array[i]);
-		}
-		printf("\n");
+int partition(int *array, int lo, int hi) {
+    int pivot = hi;
 
-		// partitioning implementation
-		// find new lo
-		while(array[lo] < array[pivot-1]) lo++;
+    while (lo < hi) {
+        while (array[lo] <= array[pivot]) lo++;
+        while (array[hi] > array[pivot]) hi--;
+        if (lo < hi) swap(&array[lo], &array[hi]);
+    }
 
-		// find new hi
-		while(hi >= 0 && array[hi] > array[pivot-1]) hi--;
-
-		printf("Lo: %d, Hi: %d\n", array[lo], array[hi]);
-		if(lo < hi)	swap(&array[lo], &array[hi]);
-		else {
-			swap(&array[lo], &array[pivot-1]);
-			break;
-		}
-	}
-	printf("\n");
-
-	return lo;
+    swap(&array[pivot], &array[hi]);
+    return hi;
 }
 
 void quickSort(int *array, int lo, int hi) {
-	if(lo-hi <= 0) return;
-	else {
-		int pivot = array[hi];
-		int pIndex = partition(array, lo, hi, pivot);
-		quickSort(array, lo, pIndex-1);
-		quickSort(array, pIndex+1, hi);
-	}
+    if (lo >= hi) return;
+    int pIndex = partition(array, lo, hi);
+    quickSort(array, lo, pIndex - 1);
+    quickSort(array, pIndex + 1, hi);
 }
 
 void main() {
 	int array[] = {26, 27, 42, 10, 14, 42, 33, 44, 35, 17};
 	int length = sizeof(array) / sizeof(int);
 
-	int lo = 0;
-	int hi = length - 2; // 35
-	int pivot = length - 1;
-
-	int i = partition(array, lo, hi, length);
-	partition(array, lo, i, i+1);
-
-	quickSort(array, 0, length-1);
-
-	for(int i = 0; i < length; i++) {
-		printf("%d ", array[i]);
-	}
-	printf("\n");
+	quickSort(array, 0, length - 1);
 }
